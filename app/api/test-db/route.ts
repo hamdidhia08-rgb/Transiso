d9a6be2045971966109943782703c487615ae57e
hamdidhia08-rgb <hamdidhia08@gmail.com>
2025-12-07 00:55:11 +0100

    first commit

--- app/api/test-db/route.ts
+++ app/api/test-db/route.ts
@@ -3,6 +3,11 @@ import { NextResponse } from 'next/server';
 import db from '@/lib/db';
 
 export async function GET() {
+
+  console.log("DB_HOST:", process.env.DB_HOST);
+  console.log("DB_USER:", process.env.DB_USER);
+  console.log("DB_NAME:", process.env.DB_NAME);
+
   try {
     const [rows] = await db.query('SELECT 1 + 1 AS result');
     return NextResponse.json({ success: true, result: rows });
