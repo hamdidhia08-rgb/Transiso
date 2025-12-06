286944d6f65707bdb1137a611b81fc883572a53e
hamdidhia08-rgb <hamdidhia08@gmail.com>
2025-12-06 23:43:49 +0100

    first commit

--- lib/db.ts
+++ lib/db.ts
@@ -3,7 +3,6 @@ import mysql from 'mysql2/promise';
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  port: Number(process.env.DB_PORT) || 3306,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
 });
